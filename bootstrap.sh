9706d150a4bdd1ec75451862e2d62194cfe02163
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-11-12 23:37:24 +0200

    Implemented task

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+brew install kind
+
+kind create cluster --name todoapp-cluster --config cluster.yml
+kind delete cluster --name todoapp-cluster
+
+kubectl apply -f .infrastructure/mysql-namespace.yml
+kubectl apply -f .infrastructure/mysql-headless-service.yml
+kubectl apply -f .infrastructure/mysql-secret.yml
+kubectl apply -f .infrastructure/mysql-configMap.yml
+kubectl apply -f .infrastructure/mysql-statefulSet.yml
+
+kubectl get pods -n mysql -o wide
